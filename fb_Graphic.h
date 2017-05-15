138e4d27332fc4b0ecbb7e8e5cebd5ee9696d1f9
Sleetag <sleetag13@gmail.com>
2017-05-15 17:00:40 +0200

    Ajout fb_Graphic.h

---
+++ fb_Graphic.h
@@ -0,0 +1,10 @@
+#ifndef __FB_GRAPHIC_H__
+#define __FB_GRAPHIC_H__
+
+#include "fb_Graphic.h"
+
+void pikatchu_move (struct fb_sess *fb,int pas);
+void pikatchu_not_move (struct fb_sess *fb);
+void startGame();
+
+#endif//__FB_GRAPHIC_H__
